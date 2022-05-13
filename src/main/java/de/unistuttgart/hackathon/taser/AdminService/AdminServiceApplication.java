95be4ab732ebd34dc9ddd213d022c2089c8629a8
LeviUniAcc <otterbach.levi@gmail.com>
2022-05-13 22:59:56 +0200

    init

---
+++ src/main/java/de/unistuttgart/hackathon/taser/AdminService/AdminServiceApplication.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.hackathon.taser.AdminService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/unistuttgart/hackathon/taser/AdminService/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.hackathon.taser.AdminService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

158f783ea1e5389380984bd96fa0bdffb5fdca32
LeviUniAcc <otterbach.levi@gmail.com>
2022-05-15 12:10:10 +0200

    added getter and setter for room

--- src/main/java/de/unistuttgart/hackathon/taser/AdminService/Room.java
+++ src/main/java/de/unistuttgart/hackathon/taser/AdminService/Room.java
@@ -14,4 +14,20 @@ public class Room {
         this.roomId = roomId;
         this.queueId = roomId;
     }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getQueueId() {
+        return queueId;
+    }
+
+    public void setQueueId(String queueId) {
+        this.queueId = queueId;
+    }
 }
